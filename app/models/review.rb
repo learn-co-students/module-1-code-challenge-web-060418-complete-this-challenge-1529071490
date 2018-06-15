7ea9c959647125732903db2c6354def78f6483f2
Susanna Kosonen <susannakosonen@Susannas-MacBook-Pro.local>
2018-06-15 11:13:46 -0400

    Susanna's code challenge answers

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,17 @@
 class Review
+    attr_accessor :customer, :restaurant, :text
+    @@all = []
+
+    def initialize(customer, restaurant, text)
+        @customer = customer
+        @restaurant = restaurant
+        @text = text
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
   
 end
 
